2fe5b9b84bf26b4fd751baed3184001349d3f37d
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-10-02 08:17:16 -0700

    Temporary /pets return data

--- modules/pets/pets.router.js
+++ modules/pets/pets.router.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const json = require('body-parser').json()
 
+// Remove when pets.services is ready.
+const {cats, dogs} = require('../../store');
+
 const Pets = require('./pets.service')
 const People = require('../people/people.service')
 
@@ -8,6 +11,9 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   // Return all pets currently up for adoption.
+
+  // Dummy test return data.
+  return [cats, dogs];
 })
 
 router.delete('/', json, (req, res) => {
