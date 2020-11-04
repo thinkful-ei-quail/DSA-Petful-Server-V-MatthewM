49aa57fe1be3d0b4724eb9ef50790eec9081fb99
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-11-04 10:09:35 -0800

    cors fix

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const {CLIENT_ORIGIN} = require('../../config');
 
 const app = express()
 
-app.use(cors({origin: CLIENT_ORIGIN}))
+app.use(cors())
 
 app.use('/people', require('../people/people.router'))
 app.use('/pets', require('../pets/pets.router'))
